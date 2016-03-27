b4b308443852a46d5ddde16ac48f5373a5d2260c
jmigual <j.marce.igual@gmail.com>
2016-03-27 18:47:07 +0200

    Moved Show declarations and finished Show Command

--- practica.hs
+++ practica.hs
@@ -11,15 +11,7 @@ data BoolExpr a =
     NOT (BoolExpr a) |
     Gt (NumExpr a) (NumExpr a) |
     Eq (NumExpr a) (NumExpr a)
-    deriving(Read, Show) 
-{-
-instance Show a => Show (BoolExpr a) where
-    show (AND a b)  = (show a) ++ " AND " ++ (show b)
-    show (OR a b)   = (show a) ++ " OR " ++ (show b)
-    show (NOT a)    = "NOT " ++ (show a)
-    show (Gt a b)   = (show a) ++ " > " ++ (show b)
-    show (Eq a b)   = (show a) ++ " = " ++ (show b)
--}
+    deriving(Read) 
 
 data NumExpr a = 
     Var String | 
@@ -28,16 +20,7 @@ data NumExpr a =
     Minus (NumExpr a) (NumExpr a) |
     Times (NumExpr a) (NumExpr a) |
     Div (NumExpr a) (NumExpr a) 
-    deriving (Read, Show)
-{-
-instance Show a => Show (NumExpr a) where
-    show (Var s)        = s
-    show (Const a)      = show a
-    show (Plus a b)     = (show a) ++ " + " ++ (show b)
-    show (Minus a b)    = (show a) ++ " - " ++ (show b)
-    show (Times a b)    = (show a) ++ " * " ++ (show b)
-    show (Div a b)      = (show a) ++ " / " ++ (show b) 
-    -}
+    deriving (Read)
 
 data Command a = 
     Assign (NumExpr a) (NumExpr a) | 
@@ -46,24 +29,70 @@ data Command a =
     Seq [Command a] | 
     Cond (BoolExpr a) (Command a) (Command a) | 
     Loop (BoolExpr a) (Command a)
-    deriving(Read, Show)
+    deriving(Read)
 
-{-instance Show a => Show (Command a) where
-     show (Assign x y)  = (show x) ++ " := " ++ (show y) ++ ";\n"
-     show (Input x)     = "INPUT " ++ (show x) ++ ";\n"
-     show (Print x)     = "PRINT " ++ (show x) ++ ";\n"
-     show (Seq [])      = []
-     show (Seq (x:xs))  = (show x) ++ ("\n") ++ (show (Seq xs))
-     show (Cond b i e)  = "IF " ++ (show b) ++ " THEN\n" ++ (show i) ++ "ELSE\n" ++ (show e) ++ "END"
-     show (Loop b c)    = "WHILE " ++ (show b) ++ "\nDO\n" ++ (show c) ++ "END\n"
--}
+instance Show a => Show (BoolExpr a) where
+    show (AND a b)  = (show a) ++ " AND " ++ (show b)
+    show (OR a b)   = (show a) ++ " OR " ++ (show b)
+    show (NOT a)    = "NOT " ++ (show a)
+    show (Gt a b)   = (show a) ++ " > " ++ (show b)
+    show (Eq a b)   = (show a) ++ " = " ++ (show b)
 
+instance Show a => Show (NumExpr a) where
+    show (Var s)        = s
+    show (Const a)      = show a
+    show (Plus a b)     = (show a) ++ " + " ++ (show b)
+    show (Minus a b)    = (show a) ++ " - " ++ (show b)
+    show (Times a b)    = (show a) ++ " * " ++ (show b)
+    show (Div a b)      = (show a) ++ " / " ++ (show b) 
+    
+instance Show a => Show (Command a) 
+    where
+        show (Assign x y)   = (show x) ++ " := " ++ (show y) ++ ";"
+        show (Input x)      = "INPUT " ++ (show x) ++ ";"
+        show (Print x)      = "PRINT " ++ (show x) ++ ";"
+        show c              = showIdent 0 0 c
+            where 
+                showIdent :: Show a => Int -> Int -> Command a -> String
+                showIdent _ _ ( Seq [])    = []
+                showIdent n i (Seq (x:xs)) 
+                    | i == 0        = (show x) ++ '\n':(showIdent n n (Seq xs))
+                    | otherwise     = ' ':(showIdent n (i-1) (Seq (x:xs)))
+                showIdent n i (Cond b ii ee) = ifS ++ (show b) ++ " THEN\n" ++ showIf ++ elseS ++ showEl 
+                    where 
+                        showIf      = showIdent (n+4) (n+4) ii
+                        showEl      = showIdent (n+4) (n+4) ee
+                        ifS         = addSpaces n "IF "
+                        elseS       = addSpaces n "ELSE\n"
+                showIdent n i (Loop b c) = whileS ++ (show b) ++ "\nDO\n" ++ loop ++ endS
+                    where
+                        whileS      = addSpaces n "WHILE "
+                        endS        = addSpaces n "END\n"
+                        loop        = showIdent (n+4) (n+4) c
+                showIdent n i c
+                    | i == 0        = show c
+                    | otherwise     = ' ':(showIdent n (i-1) c)
+
+                -- Adds n spaces
+                addSpaces :: Int -> String -> String
+                addSpaces n s
+                    | n == 0        = s
+                    | otherwise     = ' ':(addSpaces (n-1) s)
+
+-- Separates the next word from the passed String
 dropNextWord ::  String -> (String, String)
 dropNextWord s  = (w, d)
     where 
         begin       = dropWhile (==' ') s
         (w, r)      = span (/=' ') begin
         d           = dropWhile (==' ') r
+
+-- Removes the ';' and the remaining spaces or endlines
+dropNextLine :: String -> (String, String)
+dropNextLine s = (b, e)
+    where
+        (b, dirty)  = span (\x -> x /= ';' && x /= ' ' && x /= '\n') s
+        e           = dropWhile (\x -> x == ';' || x == ' ' || x == '\n') dirty
 
 -- Reads a String and returns a NumExpr (if it founds a String name
 -- creates a Var "name", otherwise creates a const "Number")
@@ -83,7 +112,7 @@ readNumExprMul s    = concatMul nums
         nums        = map readStringNum timesS
 
         concatMul :: [NumExpr a] -> NumExpr a
-        concatMul []        = error "concatMul: Llista buida"
+        concatMul []        = error "concatMul: Empty list"
         concatMul (x:[])    = x
         concatMul (x:xs)    = Times x (concatMul xs)
 
@@ -95,7 +124,7 @@ readNumExprDiv s     = concatDiv nums
         nums    = map readNumExprMul divsS
 
         concatDiv :: [NumExpr a] -> NumExpr a
-        concatDiv []        = error "concatDiv: Llista buida"
+        concatDiv []        = error "concatDiv: Empty list"
         concatDiv (x:[])    = x
         concatDiv (x:xs)    = Div x (concatDiv xs)
 
@@ -107,7 +136,7 @@ readNumExprMin s    = concatMin nums
         nums    = map readNumExprDiv minsS
 
         concatMin :: [NumExpr a] -> NumExpr a
-        concatMin []        = error "concatMin: Llista buida"
+        concatMin []        = error "concatMin: Empty list"
         concatMin (x:[])    = x
         concatMin (x:xs)    = Minus x (concatMin xs)
 
@@ -119,7 +148,7 @@ readNumExprPlu s    = concatPlu nums
         nums    = map readNumExprMin plusS
 
         concatPlu :: [NumExpr a] -> NumExpr a
-        concatPlu []        = error "concatPlu: Llista buida"
+        concatPlu []        = error "concatPlu: Empty list"
         concatPlu (x:[])    = x
         concatPlu (x:xs)    = Plus x (concatPlu xs)
 
@@ -127,19 +156,11 @@ readNumExprPlu s    = concatPlu nums
 readNumExpr :: Read a => String -> (NumExpr a, String)
 readNumExpr s = (readNumExprPlu str, r)
     where 
-        (str, r)  = takeCommand s [">", "<", "AND", "OR", "THEN", "DO"]
-
-
-{-readBoolExprGt :: Read a => String -> BoolExpr a
-readBoolExprGt s    = Gt x y
-    where
-        str         = splitOn " > " s
-        exp         = map (\x -> fst (readNumExpr x)) str
-        (x:y:[])  = exp
--}
+        (str, r)  = dropNextLine s
 
 -- Takes items until a command from the list is found
 takeCommand :: String -> [String] -> (String, String) 
+takeCommand [] _     = ("", "")
 takeCommand s xs
     | elem com xs    = ("", s)
     | expr == ""        = (com, cuar)
@@ -148,58 +169,107 @@ takeCommand s xs
         (com, cua)      = dropNextWord s
         (expr, cuar)    = takeCommand cua xs
 
-{-readBoolExprEq :: Read a => String -> (BoolExpr a, String)
-readBoolExprEq s-}
 
-readBoolExpr :: String -> (BoolExpr a, String)
-readBoolExpr s = (Gt (Var "3") (Var "4"), "")
+readBoolCom :: Read a => String -> BoolExpr a
+readBoolCom s
+    | com == ">" = Gt nExpr1 nExpr2
+    | com == "=" = Eq nExpr1 nExpr2
+    | otherwise     = error "readBoolCom: Unknown comparator"
+    where 
+        (expr1, dirty)  = takeCommand s [">", "="]
+        (com, expr2)    = dropNextWord dirty
+        nExpr1          = fst $ readNumExpr expr1
+        nExpr2          = fst $ readNumExpr expr2
+
+readBoolNot :: Read a => String -> BoolExpr a
+readBoolNot s
+    | com == "NOT"  = NOT (readBoolCom remaining)
+    | otherwise     = readBoolCom s
+    where 
+        (com, remaining)    = dropNextWord s
+
+readBoolAnd :: Read a => String -> BoolExpr a
+readBoolAnd s = concatAnd bool
+    where
+        subs    = splitOn " AND " s
+        bool    = map readBoolNot subs
+
+        concatAnd :: [BoolExpr a] -> BoolExpr a
+        concatAnd []        = error "concatAnd: Empty List"
+        concatAnd (x:[])    = x
+        concatAnd (x:xs)    = AND x (concatAnd xs)
+
+readBoolOr :: Read a => String -> BoolExpr a
+readBoolOr s = concatOr bool
+    where
+        subs    = splitOn " OR " s
+        bool    = map readBoolAnd subs
+
+        concatOr :: [BoolExpr a] -> BoolExpr a
+        concatOr []     = error "concatOr: Empty List"
+        concatOr (x:[]) = x
+        concatOr (x:xs) = OR x (concatOr xs)
+
+readBool :: Read a => String -> (BoolExpr a, String)
+readBool s = (readBoolOr expr, dirty)
+    where
+        (expr, dirty)       = takeCommand s ["THEN", "DO"]
 
 
 -- Creates a Input Command from a String
 readCommandInput :: String -> (Command a, String)
-readCommandInput s  = (Input (Var var), remaining)
+readCommandInput s  = (Input (Var var), rema)
     where
-        varBruta    = snd (dropNextWord s)              -- Remove INPUT
-        (var, rest) = span (/= ';') varBruta            -- Separate variable name
-        remaining   = dropWhile (==' ') (drop 1 rest)   -- Remove ';' and remaining spaces
+        varDirty    = snd $ dropNextWord s              -- Remove INPUT
+        (var, rema) = dropNextLine varDirty             -- Separate variable name
+
+-- Creates a Print Command from a String
+readCommandPrint :: Read a => String -> (Command a, String)
+readCommandPrint s  = (Print (Var var), rema)
+    where
+        varDirty    = snd $ dropNextWord s      -- Remove PRINT
+        (var, rema) = dropNextLine varDirty     -- Separate variable name
 
 -- Creates a If Command from a String
-readCommandIf :: String -> (Command a, String)
+readCommandIf :: Read a => String -> (Command a, String)
 readCommandIf s = (Cond b ifCom elCom, remaining)
     where 
-        noIf            = fst $ dropNextWord s      -- Remove IF 
-        (b, bRem)       = readBoolExpr noIf         -- Read boolean expression
-        thenRem         = fst $ dropNextWord bRem   -- Remove THEN
+        noIf            = snd $ dropNextWord s      -- Remove IF 
+        (b, bRem)       = readBool noIf             -- Read boolean expression
+        thenRem         = snd $ dropNextWord bRem   -- Remove THEN
         (ifCom, ifRem)  = readCommandSeq thenRem    -- Read if commands, stops when founds the ELSE
-        eRem            = fst $ dropNextWord ifRem  -- Remove ELSE
-        (elCom, elRem)  = readCommandSeq elRem      -- Read else commands, stops when founds END
-        remaining       = fst $ dropNextWord elRem  -- Remove END
+        eRem            = snd $ dropNextWord ifRem  -- Remove ELSE
+        (elCom, elRem)  = readCommandSeq eRem      -- Read else commands, stops when founds END
+        remaining       = snd $ dropNextWord elRem  -- Remove END
 
-readCommandWhile :: String -> (Command a , String)
+-- Creates a WHile Command from a String
+readCommandWhile :: Read a => String -> (Command a , String)
 readCommandWhile s      = (Loop b wCom, remaining)
     where
-        noWhile         = fst $ dropNextWord s      -- Remove WHILE
-        (b, bRem)       = readBoolExpr noWhile      -- Read boolean expression
-        dRem            = fst $ dropNextWord bRem   -- Remove DO
+        noWhile         = snd $ dropNextWord s      -- Remove WHILE
+        (b, bRem)       = readBool noWhile          -- Read boolean expression
+        dRem            = snd $ dropNextWord bRem   -- Remove DO
         (wCom, wRem)    = readCommandSeq dRem       -- Read sequence
-        remaining       = fst $ dropNextWord wRem   -- Remove END
+        remaining       = snd $ dropNextWord wRem   -- Remove END
+        
 
 
 -- Creates a Seq Command from a String
-readCommandSeq :: String -> (Command a, String)
+readCommandSeq :: Read a => String -> (Command a, String)
+readCommandSeq []       = (Seq [], [])
 readCommandSeq s
     | com == "INPUT"    = readCommandSomSeq readCommandInput s
     | com == "IF"       = readCommandSomSeq readCommandIf s
     | com == "WHILE"    = readCommandSomSeq readCommandWhile s
-  {-  | com == "DO"       =
-    | com == "PRINT"    =
-    | com == "END"      = -}
-    | otherwise         = (Seq [], [])
+    | com == "ELSE"     = (Seq [], s)
+    | com == "END"      = (Seq [], s)
+    | com == "PRINT"    = readCommandSomSeq readCommandPrint s
+    | otherwise         = error "readCommandSeq: Unknown command"
     where 
         com             = fst $ dropNextWord s
 
         -- Creates a Seq Command from a String and the selected function
-        readCommandSomSeq :: (String -> (Command a, String)) -> String -> (Command a, String)
+        readCommandSomSeq :: Read a => (String -> (Command a, String)) -> String -> (Command a, String)
         readCommandSomSeq _ [] = (Seq [], [])
         readCommandSomSeq f x  = (Seq (singleCommand:xs), remaining)
             where   
@@ -207,7 +277,7 @@ readCommandSeq s
                 (Seq xs, remaining)     = readCommandSeq remAux -- Call the main function to read another command
 
 -- Creates an AST from the input String
-readCommand :: Num a => String -> Command a
+readCommand :: Read a => String -> Command a
 readCommand s = fst $ readCommandSeq s
     
 main :: IO ()
